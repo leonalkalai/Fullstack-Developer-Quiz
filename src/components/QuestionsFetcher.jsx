39a6c83bcc7f3c898e42d3d3cde238064d8633cd
Leon Koundouras <Koundouras@hotmail.com>
2024-10-16 16:51:48 +0300

    prevent 403

--- src/components/QuestionsFetcher.jsx
+++ src/components/QuestionsFetcher.jsx
@@ -4,7 +4,7 @@ const QuestionsFetcher = ({ setQuestions }) => {
     useEffect(() => {
         const fetchQuestions = async () => {
             try {
-                const apiKey = process.env.REACT_APP_API_KEY;
+                const apiKey = import.meta.env.VITE_API_KEY;
                 console.log(apiKey);
                 const url = "https://sable-boiled-wedge.glitch.me/api/questions/";
                 const collections = ['html5_questions', 'css3_questions', 'js_questions', 'react_questions'];
