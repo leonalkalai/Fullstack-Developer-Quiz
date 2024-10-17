3c70cafdf92beab6c241de01d42286b8174490f2
Leon Koundouras <Koundouras@hotmail.com>
2024-10-17 15:11:08 +0300

    rename questionsFetche backup file

--- src/App.jsx
+++ src/App.jsx
@@ -129,11 +129,11 @@ function App(props) {
           setCategoryValue={setCategoryValue}
           questions={questionsList}
           questionsList={questionsList}
-          html5_questions={questions.html5_questions || []}
-          css3_questions={questions.css3_questions || []}
-          js_questions={questions.js_questions || []}
-          react_questions={questions.react_questions || []}
-          setQuestionsList={updateSetQuestionsList || []}
+          html5_questions={questions.html5_questions}
+          css3_questions={questions.css3_questions}
+          js_questions={questions.js_questions}
+          react_questions={questions.react_questions}
+          setQuestionsList={updateSetQuestionsList}
           showApp={showApp}
           setShowApp={setShowApp}
           chooseHome={chooseHome}
